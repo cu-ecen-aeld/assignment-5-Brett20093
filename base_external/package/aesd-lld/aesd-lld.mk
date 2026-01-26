b2394087dcaa1d6821c1fe9da42abf47d74f5b31
Brett Lange <brett.lange@colorado.edu>
2026-01-25 21:26:26 -0600

    updated modules

--- base_external/package/aesd-lld/aesd-lld.mk
+++ base_external/package/aesd-lld/aesd-lld.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_LLD_VERSION = 'd768e2fddf929d806fe1a5fb0af9ca0d00c09f69'
+AESD_LLD_VERSION = '39c15dafe7d13cfeb845382312cc67175c675cf3'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -20,6 +20,7 @@ define AESD_LLD_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/aesdlld-start-stop $(TARGET_DIR)/etc/init.d/S98lddmodules
 endef
 
 $(eval $(kernel-module))
