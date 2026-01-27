56df3d5d8be0f73faa9b76bae11b50bbc4dacc1f
Brett Lange <brett.lange@colorado.edu>
2026-01-26 19:53:00 -0600

    updated modules

--- base_external/package/aesd-lld/aesd-lld.mk
+++ base_external/package/aesd-lld/aesd-lld.mk
@@ -6,14 +6,15 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_LLD_VERSION = '39c15dafe7d13cfeb845382312cc67175c675cf3'
+AESD_LLD_VERSION = '9ed2cf4e762ab5894f3bb982f9b7e2d3063d96a8'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 AESD_LLD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-Brett20093.git'
 AESD_LLD_SITE_METHOD = git
-AESD_LLD_GIT_SUBDIR = misc-modules scull
 AESD_LLD_GIT_SUBMODULES = YES
+
+AESD_LLD_MODULE_SUBDIRS = misc-modules scull
 
 define AESD_LLD_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
